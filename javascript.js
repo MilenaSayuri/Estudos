e679019d098112bfbbc7623f86de17f79e206b1a
Milena <mirenahayakawa@gmail.com>
2023-07-19 15:16:01 -0300

    melhorias - wip 3

--- javascript.js
+++ javascript.js
@@ -1,37 +1,65 @@
-var teste = '1';
+//Array para armazenar os dados do formulario
+let dadosArmazenados = [];
 
+//Funcao para armazenar os dados do formulario
 function mostrarDadosDoFormulario() {
-    let oi = '2';
 
-    console.log(teste)
-    console.log(oi)
-    // Obtém os valores dos campos do formulário
-    let nome = document.getElementById('name').value;
-    let idade = document.getElementById('idade').value;
-    let email = document.getElementById('email').value;
-    let telefone = document.getElementById('tel').value;
-    let genero = document.querySelector('input[name="gender"]:checked').value;
-    let pais = document.getElementById('pais').value;
-    let cidade = document.getElementById('cidade').value;
+  //Crio uma variavel do tipo let e obtenho os valores dos campos do formulario
+  let nome = document.getElementById('name').value;
+  let dt_nascimento = document.getElementById('dt_nascimento').value;
+  let email = document.getElementById('email').value;
+  let telefone = document.getElementById('tel').value;
+  let genero = document.querySelector('input[name="gender"]:checked').value;
+  let pais = document.getElementById('pais').value;
+  let cidade = document.getElementById('cidade').value;
 
-    debugger
+  //Cria um objeto com os dados do formulario(uma struct para banco de dados)
+  let dadosFormulario = {
 
-    // Monta a mensagem a ser exibida com os dados do formulário
-    const mensagemExibicao = `
-      Nome: ${nome}
-      Idade: ${idade}
-      Email: ${email}
-      Telefone: ${telefone}
-      Gênero: ${genero}
-      País: ${pais}
-      Cidade: ${cidade}
-    `;
+    //'propriedade : valor'
+    nome: nome,
+    dt_nascimento: dt_nascimento,
+    email: email,
+    telefone: telefone,
+    genero: genero,
+    pais: pais,
+    cidade: cidade
+  };
 
-    // Exibe a mensagem em uma caixa de notificação
+  //Adiciona os dados do formulario ao array
+  dadosArmazenados.push(dadosFormulario);
+
+console.log(dadosArmazenados)
+
+  //Verifica se atingimos 3 conjuntos de dados armazenados
+  if (dadosArmazenados.length === 3) {
+    mostrarDadosArmazenados();
+  }
+}
+
+  //Funcao para mostrar os dados armazenado como um alerta
+  function mostrarDadosArmazenados() {
+    let mensagemExibicao = '';
+    
+    //Percorre os dados armazenados e monta a mensagem com os dados do formulario
+    dadosArmazenados.forEach((dados, index) => {
+      mensagemExibicao += `
+  Dados ${index + 1}:
+  Nome : ${dados.nome}
+  Data de nascimento : ${dados.dt_nascimento}
+  Email : ${dados.email}
+  Telefone : ${dados.telefone}
+  Gênero : ${dados.genero}
+  País : ${dados.pais}
+  Cidade : ${dados.cidade}
+  `;
+    });
     alert(mensagemExibicao);
-}
+  }
 
-window.onload = function () {
-    console.log(teste)
-    console.log(oi)
-}
+
+
+
+
+
+
